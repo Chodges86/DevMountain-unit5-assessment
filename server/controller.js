69536119306246d920b49bb0b4bbf261d40cafb7
Caleb Hodges <chodgesdev@gmail.com>
2023-01-27 21:04:00 -0600

    Part 3 completed

--- server/controller.js
+++ server/controller.js
@@ -1,6 +1,16 @@
 const dotenv = require('dotenv').config();
 const { CONNECTION_STRING } = process.env;
 
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize(CONNECTION_STRING, {
+    dialect: 'postgres',
+    dialectOptions: {
+        ssl: {
+            rejectUnauthorized: false
+        }
+    }
+})
 
 module.exports = {
     seed: (req, res) => {
@@ -13,7 +23,12 @@ module.exports = {
                 name varchar
             );
 
-            *****YOUR CODE HERE*****
+            create table cities (
+                city_id serial primary key,
+                name varchar,
+                rating integer, 
+                country_id integer not null references countries(country_id)
+            );
 
             insert into countries (name)
             values ('Afghanistan'),
